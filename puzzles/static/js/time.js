620cd14a7c88db1da1d3c5d0ee8eb5acb40c0a46
Alan Huang <cesium@mit.edu>
2022-03-15 17:41:21 -0400

    Additional minor edits

--- puzzles/static/js/time.js
+++ puzzles/static/js/time.js
@@ -17,25 +17,25 @@ function updateTimestamps() {
                 case 'F': options.month = 'long'; break;
                 case 'm': options.month = '2-digit'; break;
                 case 'n': options.month = 'numeric'; break;
-                case 'M': 
-                case 'N': 
+                case 'M':
+                case 'N':
                 case 'b': options.month = 'short'; break;
                 case 'd': options.day = '2-digit'; break;
                 case 'j': options.day = 'numeric'; break;
-                case 'G': options.hour = 'numeric'; 
-                        options.hour12 = false;
-                        break;
-                case 'H': options.hour = '2-digit'; 
-                        options.hour12 = false;
-                        break;
-                case 'g': options.hour = 'numeric'; 
-                        options.hour12 = true;
-                        break;
-                case 'h': options.hour = '2-digit'; 
-                        options.hour12 = true;
-                        break;
+                case 'G': options.hour = 'numeric';
+                          options.hour12 = false;
+                          break;
+                case 'H': options.hour = '2-digit';
+                          options.hour12 = false;
+                          break;
+                case 'g': options.hour = 'numeric';
+                          options.hour12 = true;
+                          break;
+                case 'h': options.hour = '2-digit';
+                          options.hour12 = true;
+                          break;
                 case 'i': options.minute = '2-digit'; break;
-                case 'A': 
+                case 'A':
                 case 'a': options.hour12 = 'true'; break;
                 case 'T': break;
                 case '\\': escaped = true; break;
